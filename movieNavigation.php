743548733969da6d19b91887b29eeb171560ca24
nasiru zakari <nasiruzakari51@gmail.com>
2025-03-06 10:09:01 +0000

    Added index.php and Dockerfile

--- movieNavigation.php
+++ movieNavigation.php
@@ -24,7 +24,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-          <a class="nav-link  " aria-current="page" href="movieSite.php">Home</a>
+          <a class="nav-link  " aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link  " aria-current="page" href="latestMovieSite.php">latest</a>
