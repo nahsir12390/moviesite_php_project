bea3c09377e44c21aa296d30cf057d503787f702
nasiru zakari <nasiruzakari51@gmail.com>
2025-03-06 10:43:45 +0000

    Updated database connection to use environment variables

--- movieDatabaseConn.php
+++ movieDatabaseConn.php
@@ -1,11 +1,12 @@
 <?php
-$host = "sql7.freesqldatabase.com";
-$dbname = "sql7766235";
-$username = "sql7766235";
-$password = "n9M7dnhKRP";
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$dbname = getenv("DB_NAME");
+$port = getenv("DB_PORT");
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
     
